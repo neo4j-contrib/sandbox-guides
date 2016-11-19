48a5a92ec7c3f7ca9ba85286e3bbb55afd29cb3e
johnymontana <lyonwj@gmail.com>
2016-11-18 16:07:15 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Neo4j browser guides for sandbox instances
+
+1. Edit `{USE-CASE}/{GUIDE-NAME-HERE}.adoc`
+1. `{USE-CASE}/render.sh [publish]`
+
+1. Open Neo4j Browser and run `:play http://localhost:8001/{USE-CASE}`
+
+
---
+++ us-elections-2016/render.sh
@@ -0,0 +1,21 @@
+echo "Usage: sh render.sh [publish]"
+GUIDES=..
+
+function render {
+$GUIDES/run.sh election-guide.adoc index.html +1 "$@"
+
+}
+
+if [ "$1" == "publish" ]; then
+  URL=guides.neo4j.com/sandbox/us-elections-2016
+  render http://$URL
+  s3cmd put --recursive -P *.html img s3://${URL}/
+  s3cmd put -P index.html s3://${URL}
+
+  echo "Publication Done"
+else
+  URL=localhost:8001/sandbox/us-elections-2016
+  render http://$URL
+  echo "Starting Websever at $URL Ctrl-c to stop"
+  python $GUIDES/http.py
+fi
