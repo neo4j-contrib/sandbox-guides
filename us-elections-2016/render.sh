dad0ef42f71b8b0f4ebd8cdbc14f75579dc00134
ryguyrg <ryan@ryugy.com>
2016-11-28 09:55:10 -0800

    changed to use aws if exists instead of s3cmd

--- us-elections-2016/render.sh
+++ us-elections-2016/render.sh
@@ -9,8 +9,13 @@ $GUIDES/run.sh election-guide.adoc index.html +1 "$@"
 if [ "$1" == "publish" ]; then
   URL=guides.neo4j.com/sandbox/us-elections-2016
   render http://$URL
-  s3cmd put --recursive -P *.html img s3://${URL}/
-  s3cmd put -P index.html s3://${URL}
+  if hash aws 2>/dev/null; then
+    aws s3 cp --acl public-read --recursive --exclude "*" --include "*.html" --include "*.png" --include "*.jpg" --include "*.gif" . s3://${URL}/
+    aws s3 cp --acl public-read index.html s3://${URL}
+  else
+    s3cmd put --recursive -P *.html img s3://${URL}/
+    s3cmd put -P index.html s3://${URL}
+  fi
 
   echo "Publication Done"
 else
