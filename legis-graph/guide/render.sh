7655c0aa10216e8c0b1ef42032d36061b3498fb2
johnymontana <lyonwj@gmail.com>
2017-02-08 15:46:17 -0800

    update csv url

--- legis-graph/guide/render.sh
+++ legis-graph/guide/render.sh
@@ -17,7 +17,7 @@ $GUIDES/run.sh fec-import-exerise-answers.adoc fecimportanswers.html +1 "$@"
 
 if [ "$1" == "publish" ]; then
   URL=guides.neo4j.com/sandbox/legis-graph
-  render http://$URL -a csv-url=http://guides.neo4j.com/legis-graph/data/ -a env-training
+  render http://$URL -a csv-url=http://guides.neo4j.com/sandbox/legis-graph/data/ -a env-training
   if hash aws 2>/dev/null; then
 	  aws s3 cp --acl public-read --recursive --exclude "*" --include "*.html" --include "*.png" --include "*.jpg" --include "*.gif" --include "*.csv" s3://${URL}/
 	  aws s3 cp --acl public-read index.html s3://${URL}
