2579f8a40c974d2a45e5b9b304da25160eba3d7e
johnymontana <lyonwj@gmail.com>
2017-02-16 11:39:41 -0800

    add initial version of recommendations

---
+++ recommendations/guide/render.sh
@@ -0,0 +1,24 @@
+echo "Usage: sh render.sh [publish]"
+GUIDES=../../neo4j-guides
+
+function render {
+$GUIDES/run.sh intro.adoc index.html +1 "$@"
+}
+
+if [ "$1" == "publish" ]; then
+	URL=guides.neo4j.com/sandbox/recommendations
+	render http://$URL -a csv-url=http://guides.neo4j.com/sandbox/recommendations/data/ -a env-training
+	if hash aws 2>/dev/null; then
+		aws s3 cp --acl public-read --recursive --exclude "*" --include "*.html" --include "*.png" --include "*.jpg" --include "*.gif" --include "*.csv" s3://${URL}/
+		aws s3 cp --acl public-read index.html s3://${URL}
+	else
+		s3cmd put --recursive -P *.html img s3://${URL}/ # data
+		s3cmd put -P index.html s3://${URL}
+	fi
+	echo "Publication Done"
+else
+	URL=localhost:8001/sandbox/recommendations
+	render http://$URL -a csv-url=file:/// -a env-training
+	echo "Starting webserver at $URL Ctrl-C to stop"
+	python $GUIDES/http-server.py
+fi
