3a8a1cca605c6a8547fe0a8ec5c7848eb88bcd27
johnymontana <lyonwj@gmail.com>
2017-08-25 09:27:24 -0700

    add mdm guide

--- mdm/guide/render.sh
+++ mdm/guide/render.sh
@@ -3,14 +3,6 @@ GUIDES=../../neo4j-guides
 
 function render {
 $GUIDES/run.sh index.adoc index.html +1 "$@"
-$GUIDES/run.sh intro.adoc intro.html +1 "$@"
-$GUIDES/run.sh import.adoc import.html +1 "$@"
-$GUIDES/run.sh sna.adoc sna.html +1 "$@"
-$GUIDES/run.sh contracts.adoc contracts.html +1 "$@"
-$GUIDES/run.sh exploratory.adoc exploratory.html +1 "$@"
-$GUIDES/run.sh exercises.adoc exercises.html +1 "$@"
-$GUIDES/run.sh interestingqueries.adoc interesting.html +1 "$@"
-$GUIDES/run.sh littlesis.adoc littlesis.html +1 "$@"
 }
 
 if [ "$1" == "publish" ]; then
