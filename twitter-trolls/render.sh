9a200821a09fcc314a76cba0bb5e1506a346658c
johnymontana <lyonwj@gmail.com>
2018-02-01 09:43:13 -0700

    use s3cmd in lieu of aws cli

--- twitter-trolls/render.sh
+++ twitter-trolls/render.sh
@@ -8,13 +8,8 @@ $GUIDES/run.sh index.adoc index.html +1 "$@"
 if [ "$1" == "publish" ]; then
 	URL=guides.neo4j.com/sandbox/twitter-trolls
 	render http://$URL -a csv-url=http://guides.neo4j.com/sandbox/twitter-trolls/data/ -a env-training
-	if hash aws 2>/dev/null; then
-		aws s3 cp --acl public-read --recursive --exclude "*" --include "*.html" --include "*.png" --include "*.jpg" --include "*.gif" --include "*.csv" s3://${URL}/
-		aws s3 cp --acl public-read index.html s3://${URL}
-	else
-		s3cmd put --recursive -P *.html img s3://${URL}/ # data
-		s3cmd put -P index.html s3://${URL}
-	fi
+	s3cmd put --recursive -P *.html img s3://${URL}/
+	s3cmd put -P index.html s3://${URL}
 	echo "Publication Done"
 else
 	URL=localhost:8001/sandbox/twitter-trolls
