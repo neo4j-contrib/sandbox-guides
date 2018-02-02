b39e08de49575bae926e087785174104ab873993
johnymontana <lyonwj@gmail.com>
2018-02-02 15:47:05 -0700

    guide updates

--- twitter-trolls/render.sh
+++ twitter-trolls/render.sh
@@ -8,8 +8,8 @@ $GUIDES/run.sh index.adoc index.html +1 "$@"
 if [ "$1" == "publish" ]; then
 	URL=guides.neo4j.com/sandbox/twitter-trolls
 	render http://$URL -a csv-url=http://guides.neo4j.com/sandbox/twitter-trolls/data/ -a env-training
-	s3cmd put --recursive -P *.html img s3://${URL}/
-	s3cmd put -P index.html s3://${URL}
+	s3cmd put --cf-invalidate --recursive -P *.html img s3://${URL}/
+	s3cmd put --cf-invalidate -P index.html s3://${URL}
 	echo "Publication Done"
 else
 	URL=localhost:8001/sandbox/twitter-trolls
